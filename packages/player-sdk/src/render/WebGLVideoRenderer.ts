cc6f46faa3a58b116626b5041dfa2e277cc63de0
Melnikov Aleksandr <amelnikov@telematika.com>
2026-07-01 19:29:52 +0300

    release: core@1.0.2, ui@1.0.2 (2026-07-01)

--- packages/player-sdk/src/render/WebGLVideoRenderer.ts
+++ packages/player-sdk/src/render/WebGLVideoRenderer.ts
@@ -262,10 +262,6 @@ export class WebGLVideoRenderer {
   }
 
   private fallbackTo2D() {
-    if (this.hasGLSlot) {
-      WebGLContextPool.releaseGLSlot(this);
-      this.hasGLSlot = false;
-    }
     this.gl = null;
     this.canvas2dCtx = this.canvas.getContext('2d');
     this.isFallbackMode = true;
